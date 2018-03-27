4190e7d833bf2f1b13f7e219febb43457f6724eb
Maverick Tesini <mavericktesini@gmail.com>
2018-03-27 16:13:31 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,10 +1,10 @@
 module Concerns::Findable
 
-  def find_by_name(name)
+  def self.find_by_name(name)
     all.detect {|element| element.name }
   end
 
-  def find_or_create_by_name(name)
+  def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create(name)
   end
 
